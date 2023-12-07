1f4ea3cb8a78758b908c65e952ee1abea973ea0b
sheninthjr <sheninthjr23@gmail.com>
2023-12-07 09:42:52 +0530

    Into Component

---
+++ packages/ui/src/Intro.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <>
+    <div className=''>
+        <div className='flex bg-white'>
+            <div className='text-black m-2 hover::text-cyan-200'>
+                Sheninth Jr
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Intro
